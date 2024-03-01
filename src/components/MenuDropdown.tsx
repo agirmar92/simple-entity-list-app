71557b48e784bee66dd065b452efb2f68d47b108
aegir <agir.jonsson@matsmart.se>
2024-03-01 22:26:08 +0100

    chore: update comment

--- src/components/MenuDropdown.tsx
+++ src/components/MenuDropdown.tsx
@@ -13,10 +13,10 @@ const MenuDropdown = ({
       <Button onClick={() => setIsOpen((prevValue) => !prevValue)} />
       {isOpen && (
         <div className="absolute top-11 right-0 bg-anti-flash-white rounded px-4 py-2 min-w-44 shadow-lg divide-y-2 divide-stone-400">
-          {/* TODO: Turn into buttons */}
           <button onClick={onAddContactCallback} className="py-1">
             Add contact
           </button>
+          {/* TODO: Turn into button and implement functionality */}
           <p className="py-1">Search</p>
         </div>
       )}
