acbe645444ca488d717b3e92e858bc863cbf9a83
aegir <agir.jonsson@matsmart.se>
2024-03-01 21:03:49 +0100

    feat: setup skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 function App() {
   return (
-    <>
-      <header>Amazing header</header>
-      <main>Some content</main>
-    </>
+    <div className="flex flex-col bg-anti-flash-white h-dvh">
+      <header>
+        <h1 className="text-center font-bold text-lg py-4 uppercase">
+          Your contacts
+        </h1>
+      </header>
+      <main className="p-2 flex justify-center">
+        <div className="flex justify-center items-center bg-slate-400 shadow-lg grow-1 w-full max-w-2xl text-white min-h-52 rounded">
+          list of contacts
+        </div>
+      </main>
+    </div>
   )
 }
 
